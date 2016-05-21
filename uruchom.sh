8dfe8d94499672d64705f86bfc995064097c2b62
ptutak <p.tutak@outlook.com>
2016-05-21 15:16:12 +0200

    update: kolejka-send.c, uruchom.sh

--- uruchom.sh
+++ uruchom.sh
@@ -2,13 +2,7 @@
 
 make
 
-git clone https://github.com/nathanweeks/ipcmd.git ./ipcmd-download
-
-cd ./ipcmd-download/
-make
-cd ..
-
-echo "komunikacja poprzez programy:"
+echo "komunikacja poprzez programy w C:"
 
 ./kolejka-send
 wiadomosc=`./kolejka-receive`
@@ -18,10 +12,57 @@ if [[ "${wiadomosc}" != "wyraz niepoprawny" ]]; then
 
 	wynik=`echo ${wiadomosc} | cut -d":" -f2 | aspell list -l pl`
 
-	if [[ "${wynik}" = "" ]]; then
+	if [[ "${wynik}" == "" ]]; then
 		echo "słowo poprawne w języku polskim"
 	else
 		echo "słowo niepoprawne w języku polskim"
 	fi
 fi
+echo ""
+echo "komunikacja poprzez skrypt i ipcmd:"
 
+git clone https://github.com/nathanweeks/ipcmd.git ./ipcmd-download
+
+cd ./ipcmd-download/
+make
+cd ..
+
+klucz=`./ipcmd-download/bin/ipcmd ftok ./kolejka-send 90`
+msgid=`./ipcmd-download/bin/ipcmd msgget -Q ${klucz} -e -m 0600`
+
+echo "Podaj wyraz:"
+read wyraz
+
+./ipcmd-download/bin/ipcmd msgsnd -q ${msgid} -t 255 -n ${wyraz}
+
+wyraz_wynik=`./ipcmd-download/bin/ipcmd msgrcv -q ${msgid} -t 255 -n`
+
+ipcrm -q ${msgid}
+znak=`printf "%d" "'${wyraz_wynik:0:1}"`
+if ([[ ${znak} -ge 65 ]] && [[ ${znak} -le 90 ]]) || ([[ ${znak} -ge 97 ]] && [[ ${znak} -le 122 ]]); then
+	bool=1
+	for x in `seq 1 $((${#wyraz_wynik}-1))`
+	do
+		znak=`printf "%d" "'${wyraz_wynik:${x}:1}"`
+		
+		if [[ ${znak} -lt 0 ]] || [[ ${znak} -gt 127 ]]; then
+			bool=0
+		fi
+	done
+	if [[ ${bool} == 1 ]]; then
+		echo "wyraz poprawny:${wyraz_wynik}"
+		
+		wynik=`echo "${wyraz_wynik}" | aspell list -l pl`
+		
+		if [[ "${wynik}" == "" ]]; then
+			echo "słowo poprawne w języku polskim"
+		else
+			echo "słowo niepoprawne w języku polskim"
+		fi
+
+	else
+		echo "wyraz niepoprawny"
+	fi
+else
+	echo "wyraz niepoprawny"
+fi
